5c12b77741c55429cfb67ba2c7fca52db63d8454
meetthehorizon <kshitij.sharma.cse22@itbhu.ac.in>
2024-07-12 01:34:51 +0530

    12/07/24 01:34

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/sublime-text/Packages/User/*.sublime-snippet ./snippets
-git add .
-
-current_datetime=$(date "+%d/%m/%y %H:%M")
-git commit -m "$current_datetime"
-
-git push origin main
