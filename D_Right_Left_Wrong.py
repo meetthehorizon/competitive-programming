4c01b739be870710f284ca0a5da0d1da21557756
meetthehorizon <kshitij.sharma.cse22@itbhu.ac.in>
2024-09-28 00:14:42 +0530

    28/09/24 00:14

--- D_Right_Left_Wrong.py
+++ D_Right_Left_Wrong.py
@@ -1,1 +1,17 @@
+# Kshitij Sharma
+# github: meetthehorizon
+#    /\_/\
+#   (= ._.)
+#   / >  \>
+
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def solve():
+    
+
+is_test = True
+
+t = 1 if not is_test else int(input())
+for _ in range(t): solve()
     
